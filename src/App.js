4a3f8c52059b49f57c0f728c0670186aa2b136f1
noorullahazizidxb <146918490+noorullahazizidxb@users.noreply.github.com>
2024-04-27 00:29:13 +0430

    Basic Structure Completed
    i have completed the basic structure of the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Home from './Pages/Home/Home';
+import Admission from './Pages/Admissions/Admission';
+import Finance from './Pages/Finance/Finance';
+import Accessories from './Pages/Accessories/Accessories';
+import MiniDrawer from './Components/MiniDrawer';
+
 
 function App() {
   return (
- <>
- this will be our react app
- </>
+    <BrowserRouter>
+    <Routes>      
+      <Route exact path="/" element={<MiniDrawer Content={<Home/>}/>} />
+      <Route path="/Admission" element={<MiniDrawer Content={<Admission/>}/>} />
+      <Route path="/finance" element={<MiniDrawer Content={<Finance/>}/>} />
+      <Route path="/settings" element={<MiniDrawer Content={<Accessories/>}/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
